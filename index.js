94cbb94e1bdc0e7e7dd6a167405ea8af6eb6a8d7
Anna-Ashton <ann.karanja@student.moringaschool.com>
2022-08-08 17:29:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards (names) {
+    let thankCards= []
+    for (let i=0; i<names.length; i++) {
+        thankCards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+return thankCards;
+    }
+    function countDown( start ) {
+        let countUp = 0;
+        while (countUp < 10) {
+            console.log(countUp++);
+        }
+        console.log( start );
+    }
